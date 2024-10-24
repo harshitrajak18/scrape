d6de59c3665b5388fb0284ee93f8971cdec7b5d7
Harshit Rajak <106432378+harshitrajak18@users.noreply.github.com>
2024-10-24 22:44:19 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ha2ghtfzpgcc39cb)u*6(^di&c9(oi9%(9zfp3^(bjfo)+!b#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["scrape.onrender.com"]
 
 
 # Application definition
